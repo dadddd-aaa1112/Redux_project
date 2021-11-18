1179c7ae3e38e182bbbe4c7f96dc025847b1820a
dadddd-aaa1112 <74870737+dadddd-aaa1112@users.noreply.github.com>
2021-11-18 15:00:39 +0300

    change in diff files active action

--- reducers/cityActive.js
+++ reducers/cityActive.js
@@ -1,6 +1,6 @@
 export default function (state = null, action) {
   switch (action.type) {
-    case "CAR_SELECTED":
+    case "City_slected":
       return action.payload;
       break;
 
--- reducers/index.js
+++ reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import CarsReducers from "./car";
 import CityReducers from "./city";
 import CarActive from "./carActive";
+import CityActive from "./cityActive";
 
 const allReducers = combineReducers({
   cars: CarsReducers,
   activeCar: CarActive,
-  citys: CityReducers
+  citys: CityReducers,
+  activeCity: CityActive
 });
 
 export default allReducers;
